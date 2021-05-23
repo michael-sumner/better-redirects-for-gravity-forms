fc99b5e6a827ea1704003029d64daaadebe2d1f3
Michael Sumner <mic@micsumner.com>
2021-05-23 12:41:15 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,80 @@
+=== Better Redirects for Gravity Forms ===
+Contributors: @xmic
+Donate link: https://ko-fi.com/michaelsumner
+Tags: gravity, forms, redirects, seo, 404, form, gf
+Requires at least: 4.0
+Requires PHP: 5.6
+Tested up to: 5.7.2
+Stable tag: 1.0.0
+License: GPL-2.0+
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Avoids 404 errors in your form confirmation redirects. Specify your confirmation redirect URL, so that form redirects will never 404 again.
+
+== Description ==
+
+// todo
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0.0 =
+* A change since the previous version.
+* Another change.
+
+
+
+
+
+
+== A brief Markdown Example ==
+
+Ordered list:
+
+1. Some feature
+1. Another feature
+1. Something else about the plugin
+
+Unordered list:
+
+* something
+* something else
+* third thing
+
+Here's a link to [WordPress](http://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
+Titles are optional, naturally.
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
+
+Markdown uses email style notation for blockquotes and I've been told:
+> Asterisks for *emphasis*. Double it up  for **strong**.
+
+`<?php code(); // goes in backticks ?>`
---
+++ js/admin.js
@@ -0,0 +1,124 @@
+/**
+ * @output wp-includes/js/wplink.js
+ */
+
+ /* global wpLink */
+
+ ( function( $, wpLinkL10n, wp ) {
+
+    // todo on load if values exist.
+    if(
+        $('#betterRedirectsGf-field-value-post-id').length &&
+        $('#betterRedirectsGf-field-value-post-url').length
+    ) {
+
+        var textarea;
+
+        function hideResults() {
+            $('.js-c-betterRedirectsGf-result').addClass('gform-visually-hidden');
+            showButton();
+        }
+        function showResults() {
+            $('.js-c-betterRedirectsGf-result').removeClass('gform-visually-hidden');
+            hideButton();
+        }
+
+        function hideButton() {
+            $('.js-c-betterRedirectsGf-button-selectLink').addClass('gform-visually-hidden').prop('disabled', true);
+        }
+        function showButton() {
+            $('.js-c-betterRedirectsGf-button-selectLink').removeClass('gform-visually-hidden').prop('disabled', false);
+        }
+
+        function updateGFormSettingValues(postId, url) {
+            console.log('postId', postId)
+            console.log('url', url)
+            $('#betterRedirectsGf-field-value-post-id').val(postId);
+            $('#betterRedirectsGf-field-value-post-url').val(url);
+        }
+
+        // check gform setting values, exist.
+        if($('#betterRedirectsGf-field-value-post-id').val()) {
+            showResults();
+        } else {
+            hideResults();
+        }
+        if($('#betterRedirectsGf-field-value-post-url').val()) {
+            showResults();
+        } else {
+            hideResults();
+        }
+
+        $(document.body).on('click', '.js-c-betterRedirectsGf-button-selectLink, .js-c-betterRedirectsGf-result', function() {
+            wpLink.open();
+            textarea = wpLink.textarea['value'];
+        });
+        $(document.body).on('click', '.js-c-betterRedirectsGf-result-remove', function(event) {
+            hideResults();
+            updateGFormSettingValues('','');
+            event.stopPropagation();
+        });
+
+        $(document.body).on('click', '#wp-link-submit', function() {            
+            // override the wpLink.htmlUpdate() which has inputted the link in the available textarea
+            wpLink.textarea['value'] = textarea;
+
+            // get url from wpLink
+            var url, params;
+            url = wpLink.getAttrs().href;
+    
+            params = {};
+    
+            if(url) {
+                params.url = url;
+            }
+    
+            getPostIdFromUrl($(this), params);
+    
+            function getPostIdFromUrl(element, params) {
+    
+                formData = {
+                    'post_url'           : params.url,
+                    'action'             : 'better_redirects_gf',
+                    '_ajax_nonce'        : better_redirects_gf.nonce,
+                };
+        
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    url: better_redirects_gf.ajax_url,
+                    data: formData,
+                    beforeSend: function() {
+                        element.addClass('button-disabled').prop('disabled', true);
+                    },
+                    success: function (response) {
+                        if(response.success) {
+                            var postId;
+                            postId = response.data.post_id;
+
+                            // update gform setting values.
+                            $('#betterRedirectsGf-field-value-post-id').val(postId);
+                            $('#betterRedirectsGf-field-value-post-url').val(url);
+
+                            // update the [name=_gform_setting_url]
+                            $('[name=_gform_setting_url]').val(url);
+    
+                            // update .js-c-betterRedirectsGf-result html
+                            $('.js-c-betterRedirectsGf-result-id').html(postId);
+                            $('.js-c-betterRedirectsGf-result-url').attr('href', url).html(url);
+
+
+                            showResults();
+                        }
+                    },
+                    complete: function() {
+                        element.removeClass('button-disabled').prop('disabled', false);
+                    }
+                });
+            }
+        
+        });
+    }
+
+
+})( jQuery, window.wpLinkL10n, window.wp );
